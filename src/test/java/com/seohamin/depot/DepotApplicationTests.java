b2d7852cee30ba53f2509711f9001053b356577b
ggm77 <shmshm86@naver.com>
2026-05-25 15:07:40 +0900

    first commit

---
+++ src/test/java/com/seohamin/depot/DepotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seohamin.depot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
